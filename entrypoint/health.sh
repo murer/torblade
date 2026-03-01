02bf3eb88c465c2486d814595b31ae8c7e573723
murer <842157+murer@users.noreply.github.com>
2026-03-01 13:17:53 -0300

    x

--- entrypoint/health.sh
+++ entrypoint/health.sh
@@ -1,15 +1,25 @@
 #!/bin/bash -xe
 
-mkdir -p /var/torblade
-
 if [ -z "$TORBLADE_SERVICE" ]; then
     echo 'TORBLADE_SERVICE is required'
     false
 else
-    if [ ! -f /var/torblade/health.last ] || find /var/torblade/health.last -mmin +1 | grep '.\+'; then
+
+    function _check() {
         echo "fail" > /var/torblade/health.last
         exec /opt/torblade/tb.$TORBLADE_SERVICE.sh health
         echo "success" > /var/torblade/health.last
+    }
+
+    mkdir -p /var/torblade
+    [ ! -f /var/torblade/health.created ] && touch /var/torblade/health.created
+    [ ! -f /var/torblade/health.last ] && touch /var/torblade/health.last
+
+    if find /var/torblade/health.created -mmin -2 | grep '.\+'; then
+        _check
+    elif find /var/torblade/health.last -mmin +5 | grep '.\+'; then
+        _check
     fi
     grep success /var/torblade/health.last
+    
 fi
