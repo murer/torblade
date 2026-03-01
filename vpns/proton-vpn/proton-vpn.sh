8946ca28fcb2b477411057c10fe25f08fe22cb56
murer <murer@sample>
2026-03-01 18:19:20 -0300

    x

---
+++ vpns/proton-vpn/proton-vpn.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -xe
+
+function cmd_install_protonvpn() {
+    mkdir -p /etc/apt/hardkeys
+    wget -q -O - https://repo.protonvpn.com/debian/public_key.asc | gpg --dearmor > /etc/apt/hardkeys/protonvpn-stable-archive-keyring.gpg
+    echo "deb [signed-by=/etc/apt/hardkeys/protonvpn-stable-archive-keyring.gpg] https://repo.protonvpn.com/debian stable main" > /etc/apt/sources.list.d/proton-vpn.list
+    apt update
+    apt -y install proton-vpn-gtk-app
+
+    if [[ "x$hexblade_user" != "x" ]]; then
+        cmd_group "$hexblade_user"
+    elif [[ "x$UID" == "x0" && "x$SUDO_USER" != "x" && "x$SUDO_UID" != "x0" ]]; then
+        cmd_group "$SUDO_USER"
+    elif [[ "x$UID" != "x0" ]]; then
+        cmd_group "$USER"
+    fi
+}
+
+function cmd_install_dhpcd() {
+    true
+}
+
+function cmd_install_all() {
+    cmd_install_protonvpn
+    cmd_install_dhpcd
+}
+
+cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"
