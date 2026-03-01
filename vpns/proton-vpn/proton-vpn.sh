ef9fefbbfbfdf9584eefdd93265a288b892f4376
murer <murer@sample>
2026-03-01 18:28:57 -0300

    x

--- vpns/proton-vpn/proton-vpn.sh
+++ vpns/proton-vpn/proton-vpn.sh
@@ -27,4 +27,8 @@ function cmd_install_all() {
     cmd_install_protonvpn
 }
 
+function cmd_start() {
+    [ "x$UID" != "x0" ]
+}
+
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"
